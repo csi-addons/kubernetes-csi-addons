1b6dc160d5a93dfd97e15ab0a34b507e27a05e14
Rakshith R <rar@redhat.com>
2021-12-10 15:35:25 +0530

    api: kubebuilder create api reclaimspacejob
    kubebuilder create api --group csiaddons --version v1alpha1 --kind ReclaimSpaceJob
    
    Signed-off-by: Rakshith R <rar@redhat.com>

--- main.go
+++ main.go
@@ -85,6 +85,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "CSIAddonsNode")
 		os.Exit(1)
 	}
+	if err = (&controllers.ReclaimSpaceJobReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ReclaimSpaceJob")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
