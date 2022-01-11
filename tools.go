49d101b707dedbccaf798e555c2b8607d0cb458b
Niels de Vos <ndevos@redhat.com>
2022-01-11 16:24:09 +0100

    tools: build `operator-sdk` from the `vendor/` directory
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- tools.go
+++ tools.go
@@ -25,6 +25,7 @@ limitations under the License.
 package main
 
 import (
+	_ "github.com/operator-framework/operator-sdk/cmd/operator-sdk"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/kustomize/v4"
