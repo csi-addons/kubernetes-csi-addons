cfb4fb90d877645e7af28e6f805b677974ee48d5
Rakshith R <rar@redhat.com>
2022-08-12 17:00:24 +0530

    sidecar: add volume replication service setup
    adding sidecar service for the volume replication
    service.
    
    Signed-off-by: Rakshith R <rar@redhat.com>

--- sidecar/main.go
+++ sidecar/main.go
@@ -96,6 +96,7 @@ func main() {
 	sidecarServer.RegisterService(service.NewIdentityServer(csiClient.GetGRPCClient()))
 	sidecarServer.RegisterService(service.NewReclaimSpaceServer(csiClient.GetGRPCClient(), kubeClient, *stagingPath))
 	sidecarServer.RegisterService(service.NewNetworkFenceServer(csiClient.GetGRPCClient(), kubeClient))
+	sidecarServer.RegisterService(service.NewReplicationServer(csiClient.GetGRPCClient(), kubeClient))
 
 	sidecarServer.Start()
 }
