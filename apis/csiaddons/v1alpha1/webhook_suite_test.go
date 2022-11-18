bbc343351e3f3424d0d2f5fe1a92e9b0f60dcc12
Madhu Rajanna <madhupr007@gmail.com>
2022-11-18 13:16:40 +0100

    webhook: add webhooks for reclaimSpacecronJob
    Add webhook for reclaimspacecronjobs object to
    avoid users from changing the PVC once created.
    Below is the command to generate webhook for
    reclaimspacecronjobs object
    
    ```bash
    $ operator-sdk create webhook --group csiaddons
      --version v1alpha1 --kind ReclaimSpaceCronJob
      --programmatic-validation
    ```
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- apis/csiaddons/v1alpha1/webhook_suite_test.go
+++ apis/csiaddons/v1alpha1/webhook_suite_test.go
@@ -105,6 +105,9 @@ var _ = BeforeSuite(func() {
 	err = (&ReclaimSpaceJob{}).SetupWebhookWithManager(mgr)
 	Expect(err).NotTo(HaveOccurred())
 
+	err = (&ReclaimSpaceCronJob{}).SetupWebhookWithManager(mgr)
+	Expect(err).NotTo(HaveOccurred())
+
 	//+kubebuilder:scaffold:webhook
 
 	go func() {
