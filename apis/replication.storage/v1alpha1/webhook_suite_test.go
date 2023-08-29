a0516f64cf714728bd4e7a8062c318a92b06cf0e
Riya Singhal <rsinghal@redhat.com>
2023-08-29 14:47:27 +0530

    rebase: replace MetricsBindAddress with Metrics
    Signed-off-by: Riya Singhal <rsinghal@redhat.com>

--- apis/replication.storage/v1alpha1/webhook_suite_test.go
+++ apis/replication.storage/v1alpha1/webhook_suite_test.go
@@ -29,6 +29,8 @@ import (
 	. "github.com/onsi/gomega"
 
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
 	//+kubebuilder:scaffold:imports
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/rest"
@@ -90,12 +92,12 @@ var _ = BeforeSuite(func() {
 	// start webhook server using Manager
 	webhookInstallOptions := &testEnv.WebhookInstallOptions
 	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
-		Scheme:             scheme,
-		Host:               webhookInstallOptions.LocalServingHost,
-		Port:               webhookInstallOptions.LocalServingPort,
-		CertDir:            webhookInstallOptions.LocalServingCertDir,
-		LeaderElection:     false,
-		MetricsBindAddress: "0",
+		Scheme:         scheme,
+		Host:           webhookInstallOptions.LocalServingHost,
+		Port:           webhookInstallOptions.LocalServingPort,
+		CertDir:        webhookInstallOptions.LocalServingCertDir,
+		LeaderElection: false,
+		Metrics:        metricsserver.Options{BindAddress: "0"},
 	})
 	Expect(err).NotTo(HaveOccurred())
 
