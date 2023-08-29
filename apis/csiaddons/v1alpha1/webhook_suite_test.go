a0b1d8829f4ee7ec07e0113b826f2c3735616db3
Riya Singhal <rsinghal@redhat.com>
2023-08-29 15:04:14 +0530

    rebase: updated port, host, certDir as a field of struct server than manager
    Signed-off-by: Riya Singhal <rsinghal@redhat.com>

--- apis/csiaddons/v1alpha1/webhook_suite_test.go
+++ apis/csiaddons/v1alpha1/webhook_suite_test.go
@@ -38,6 +38,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
@@ -92,11 +93,13 @@ var _ = BeforeSuite(func() {
 	webhookInstallOptions := &testEnv.WebhookInstallOptions
 	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
 		Scheme:         scheme,
-		Host:           webhookInstallOptions.LocalServingHost,
-		Port:           webhookInstallOptions.LocalServingPort,
-		CertDir:        webhookInstallOptions.LocalServingCertDir,
 		LeaderElection: false,
 		Metrics:        metricsserver.Options{BindAddress: "0"},
+		WebhookServer: webhook.NewServer(webhook.Options{
+			Host:    webhookInstallOptions.LocalServingHost,
+			Port:    webhookInstallOptions.LocalServingPort,
+			CertDir: webhookInstallOptions.LocalServingCertDir,
+		}),
 	})
 	Expect(err).NotTo(HaveOccurred())
 
--- apis/replication.storage/v1alpha1/webhook_suite_test.go
+++ apis/replication.storage/v1alpha1/webhook_suite_test.go
@@ -29,8 +29,6 @@ import (
 	. "github.com/onsi/gomega"
 
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
-	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
-
 	//+kubebuilder:scaffold:imports
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/rest"
@@ -39,6 +37,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
@@ -93,11 +93,13 @@ var _ = BeforeSuite(func() {
 	webhookInstallOptions := &testEnv.WebhookInstallOptions
 	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
 		Scheme:         scheme,
-		Host:           webhookInstallOptions.LocalServingHost,
-		Port:           webhookInstallOptions.LocalServingPort,
-		CertDir:        webhookInstallOptions.LocalServingCertDir,
 		LeaderElection: false,
 		Metrics:        metricsserver.Options{BindAddress: "0"},
+		WebhookServer: webhook.NewServer(webhook.Options{
+			Port:    webhookInstallOptions.LocalServingPort,
+			Host:    webhookInstallOptions.LocalServingHost,
+			CertDir: webhookInstallOptions.LocalServingCertDir,
+		}),
 	})
 	Expect(err).NotTo(HaveOccurred())
 
