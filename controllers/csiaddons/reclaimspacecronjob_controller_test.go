40b63b8e672f9374cfd9e9722ec32e528274a379
karthik-us <ksubrahm@redhat.com>
2024-02-16 10:46:55 +0530

    test: unit test for getNextSchedule
    Adding unit test for getNextSchedule function in the
    reclaimspacecronjob_controller.
    
    Signed-off-by: karthik-us <ksubrahm@redhat.com>

--- controllers/csiaddons/reclaimspacecronjob_controller_test.go
+++ controllers/csiaddons/reclaimspacecronjob_controller_test.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 	"time"
 
@@ -25,6 +26,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 )
 
 func TestGetScheduledTimeForRSJob(t *testing.T) {
@@ -104,6 +106,108 @@ func TestGetScheduledTimeForRSJob(t *testing.T) {
 			fmt.Println(tt.want, got)
 			if tt.want != nil {
 				assert.Equal(t, tt.want.Format(time.RFC3339), got.Format(time.RFC3339))
+			}
+		})
+	}
+}
+
+func TestGetNextSchedule(t *testing.T) {
+	now := time.Now()
+	type args struct {
+		rsCronJob *csiaddonsv1alpha1.ReclaimSpaceCronJob
+		now       time.Time
+	}
+	tests := []struct {
+		name         string
+		args         args
+		lastMissed   time.Time
+		nextSchedule time.Time
+		wantErr      bool
+	}{
+		{
+			name: "Valid schedule, no deadline, last schedule time exists",
+			args: args{
+				rsCronJob: &csiaddonsv1alpha1.ReclaimSpaceCronJob{
+					Spec: csiaddonsv1alpha1.ReclaimSpaceCronJobSpec{
+						Schedule: "0 0 * * *",
+					},
+					Status: csiaddonsv1alpha1.ReclaimSpaceCronJobStatus{
+						LastScheduleTime: &metav1.Time{Time: now.Add(-time.Hour)},
+					},
+				},
+				now: now,
+			},
+			lastMissed:   time.Time{},
+			nextSchedule: time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local).Add(time.Hour * 24),
+			wantErr:      false,
+		},
+		{
+			name: "Valid schedule, deadline not exceeded, last schedule time exists",
+			args: args{
+				rsCronJob: &csiaddonsv1alpha1.ReclaimSpaceCronJob{
+					Spec: csiaddonsv1alpha1.ReclaimSpaceCronJobSpec{
+						Schedule:                "0 0 * * *",
+						StartingDeadlineSeconds: ptr.To(int64(3600)),
+					},
+					Status: csiaddonsv1alpha1.ReclaimSpaceCronJobStatus{
+						LastScheduleTime: &metav1.Time{Time: now.Add(-time.Hour)},
+					},
+				},
+				now: now,
+			},
+			lastMissed:   time.Time{},
+			nextSchedule: time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local).Add(time.Hour * 24),
+			wantErr:      false,
+		},
+		{
+			name: "Valid schedule, deadline exceeded, last schedule time exists, missed schedules < 100",
+			args: args{
+				rsCronJob: &csiaddonsv1alpha1.ReclaimSpaceCronJob{
+					Spec: csiaddonsv1alpha1.ReclaimSpaceCronJobSpec{
+						Schedule:                "*/1 * * * *",
+						StartingDeadlineSeconds: ptr.To(int64(6000)),
+					},
+					Status: csiaddonsv1alpha1.ReclaimSpaceCronJobStatus{
+						LastScheduleTime: &metav1.Time{Time: now.Add(-time.Hour * 2)},
+					},
+				},
+				now: now,
+			},
+			lastMissed:   time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), 0, 0, time.Local),
+			nextSchedule: time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), 0, 0, time.Local).Add(time.Minute),
+			wantErr:      false,
+		},
+		{
+			name: "Valid schedule, deadline exceeded, last schedule time exists, missed schedules > 100",
+			args: args{
+				rsCronJob: &csiaddonsv1alpha1.ReclaimSpaceCronJob{
+					Spec: csiaddonsv1alpha1.ReclaimSpaceCronJobSpec{
+						Schedule:                "*/1 * * * *",
+						StartingDeadlineSeconds: ptr.To(int64(6060)),
+					},
+					Status: csiaddonsv1alpha1.ReclaimSpaceCronJobStatus{
+						LastScheduleTime: &metav1.Time{Time: now.Add(-time.Hour * 2)},
+					},
+				},
+				now: now,
+			},
+			lastMissed:   time.Time{},
+			nextSchedule: time.Time{},
+			wantErr:      true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotLastMissed, gotNextSchedule, err := getNextSchedule(tt.args.rsCronJob, tt.args.now)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getNextSchedule() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotLastMissed, tt.lastMissed) {
+				t.Errorf("getNextSchedule() got last missed = %v, want %v", gotLastMissed, tt.lastMissed)
+			}
+			if !reflect.DeepEqual(gotNextSchedule, tt.nextSchedule) {
+				t.Errorf("getNextSchedule() got next schedule = %v, want %v", gotNextSchedule, tt.nextSchedule)
 			}
 		})
 	}
