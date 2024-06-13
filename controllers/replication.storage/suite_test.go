aaeb835b71a2a98c4658cdd69cb9443285497a04
Madhu Rajanna <madhupr007@gmail.com>
2024-06-13 11:06:00 +0200

    api: scaffolding for volumegroupreplication
    adding scaffolding for volumegroupreplication
    using below commands
    
    ```
    cp cmd/manager/main.go .
    kubebuilder create api --group replication.storage \
    --version v1alpha1 --kind VolumeGroupReplication
    mv main.go cmd/manager/
    make generate
    make manifests
    ```
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- controllers/replication.storage/suite_test.go
+++ controllers/replication.storage/suite_test.go
@@ -67,6 +67,9 @@ var _ = BeforeSuite(func() {
 	err = replicationv1alpha1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
+	err = replicationv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
 	// +kubebuilder:scaffold:scheme
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
