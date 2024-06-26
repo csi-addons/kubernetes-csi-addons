0fa344dd3cc10c9c4a75c9def243f51ef656dfc7
Niraj Yadav <niryadav@redhat.com>
2024-06-26 19:31:23 +0530

    Migrate project from go v3 layout to v4
    Signed-off-by: Niraj Yadav <niryadav@redhat.com>

---
+++ internal/controller/csiaddons/reclaimspacecronjob_controller_test.go
@@ -0,0 +1,214 @@
+/*
+Copyright 2022 The Kubernetes-CSI-Addons Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	csiaddonsv1alpha1 "github.com/csi-addons/kubernetes-csi-addons/api/csiaddons/v1alpha1"
+
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+)
+
+func TestGetScheduledTimeForRSJob(t *testing.T) {
+	type args struct {
+		rsJob *csiaddonsv1alpha1.ReclaimSpaceJob
+	}
+	scheduledTime := time.Now()
+	scheduledTimeString := scheduledTime.Format(time.RFC3339)
+	tests := []struct {
+		name    string
+		args    args
+		want    *time.Time
+		wantErr bool
+	}{
+		{
+			name: "empty scheduled time string",
+			args: args{
+				rsJob: &csiaddonsv1alpha1.ReclaimSpaceJob{
+					ObjectMeta: metav1.ObjectMeta{
+						Annotations: map[string]string{
+							scheduledTimeAnnotation: "",
+						},
+					},
+				},
+			},
+			want:    nil,
+			wantErr: false,
+		},
+		{
+			name: "invalid scheduled timestring",
+			args: args{
+				rsJob: &csiaddonsv1alpha1.ReclaimSpaceJob{
+					ObjectMeta: metav1.ObjectMeta{
+						Annotations: map[string]string{
+							scheduledTimeAnnotation: "abc",
+						},
+					},
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "missing annotation",
+			args: args{
+				rsJob: &csiaddonsv1alpha1.ReclaimSpaceJob{
+					ObjectMeta: metav1.ObjectMeta{
+						Annotations: map[string]string{},
+					},
+				},
+			},
+			want:    nil,
+			wantErr: false,
+		},
+		{
+			name: "valid scheduled timestring",
+			args: args{
+				rsJob: &csiaddonsv1alpha1.ReclaimSpaceJob{
+					ObjectMeta: metav1.ObjectMeta{
+						Annotations: map[string]string{
+							scheduledTimeAnnotation: scheduledTimeString,
+						},
+					},
+				},
+			},
+			want:    &scheduledTime,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getScheduledTimeForRSJob(tt.args.rsJob)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getScheduledTimeForRSJob() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			fmt.Println(tt.want, got)
+			if tt.want != nil {
+				assert.Equal(t, tt.want.Format(time.RFC3339), got.Format(time.RFC3339))
+			}
+		})
+	}
+}
+
+func TestGetNextSchedule(t *testing.T) {
+	now := time.Now()
+	expectedLastMissed := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
+	type args struct {
+		rsCronJob *csiaddonsv1alpha1.ReclaimSpaceCronJob
+		now       time.Time
+	}
+	tests := []struct {
+		name         string
+		args         args
+		lastMissed   time.Time
+		nextSchedule time.Time
+		wantErr      bool
+	}{
+		{
+			name: "Valid schedule, no deadline, last schedule time exists",
+			args: args{
+				rsCronJob: &csiaddonsv1alpha1.ReclaimSpaceCronJob{
+					Spec: csiaddonsv1alpha1.ReclaimSpaceCronJobSpec{
+						Schedule: "0 0 * * *",
+					},
+					Status: csiaddonsv1alpha1.ReclaimSpaceCronJobStatus{
+						LastScheduleTime: &metav1.Time{Time: now.Add(-24 * time.Hour).Truncate(24 * time.Hour).Local()},
+					},
+				},
+				now: now,
+			},
+			lastMissed:   expectedLastMissed,
+			nextSchedule: time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local).Add(time.Hour * 24),
+			wantErr:      false,
+		},
+		{
+			name: "Valid schedule, deadline not exceeded, last schedule time exists",
+			args: args{
+				rsCronJob: &csiaddonsv1alpha1.ReclaimSpaceCronJob{
+					Spec: csiaddonsv1alpha1.ReclaimSpaceCronJobSpec{
+						Schedule:                "0 0 * * *",
+						StartingDeadlineSeconds: ptr.To(int64(3600)),
+					},
+					Status: csiaddonsv1alpha1.ReclaimSpaceCronJobStatus{
+						LastScheduleTime: &metav1.Time{Time: now.Add(-24 * time.Hour).Truncate(24 * time.Hour).Local()},
+					},
+				},
+				now: now,
+			},
+			lastMissed:   expectedLastMissed,
+			nextSchedule: time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local).Add(time.Hour * 24),
+			wantErr:      false,
+		},
+		{
+			name: "Valid schedule, deadline exceeded, last schedule time exists, missed schedules < 100",
+			args: args{
+				rsCronJob: &csiaddonsv1alpha1.ReclaimSpaceCronJob{
+					Spec: csiaddonsv1alpha1.ReclaimSpaceCronJobSpec{
+						Schedule:                "*/1 * * * *",
+						StartingDeadlineSeconds: ptr.To(int64(6000)),
+					},
+					Status: csiaddonsv1alpha1.ReclaimSpaceCronJobStatus{
+						LastScheduleTime: &metav1.Time{Time: now.Add(-time.Hour * 2)},
+					},
+				},
+				now: now,
+			},
+			lastMissed:   time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), 0, 0, time.Local),
+			nextSchedule: time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), 0, 0, time.Local).Add(time.Minute),
+			wantErr:      false,
+		},
+		{
+			name: "Valid schedule, deadline exceeded, last schedule time exists, missed schedules > 100",
+			args: args{
+				rsCronJob: &csiaddonsv1alpha1.ReclaimSpaceCronJob{
+					Spec: csiaddonsv1alpha1.ReclaimSpaceCronJobSpec{
+						Schedule:                "*/1 * * * *",
+						StartingDeadlineSeconds: ptr.To(int64(6060)),
+					},
+					Status: csiaddonsv1alpha1.ReclaimSpaceCronJobStatus{
+						LastScheduleTime: &metav1.Time{Time: now.Add(-time.Hour * 2)},
+					},
+				},
+				now: now,
+			},
+			lastMissed:   time.Time{},
+			nextSchedule: time.Time{},
+			wantErr:      true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotLastMissed, gotNextSchedule, err := getNextSchedule(tt.args.rsCronJob, tt.args.now)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getNextSchedule() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !gotLastMissed.Equal(tt.lastMissed) && !gotLastMissed.Equal(time.Time{}) {
+				t.Errorf("getNextSchedule() got last missed = %v, want %v", gotLastMissed, tt.lastMissed)
+			}
+			if !gotNextSchedule.Equal(tt.nextSchedule) {
+				t.Errorf("getNextSchedule() got next schedule = %v, want %v", gotNextSchedule, tt.nextSchedule)
+			}
+		})
+	}
+}
---
+++ internal/controller/replication.storage/volumereplicationclass_test.go
@@ -0,0 +1,78 @@
+/*
+Copyright 2022 The Kubernetes-CSI-Addons Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"testing"
+
+	replicationv1alpha1 "github.com/csi-addons/kubernetes-csi-addons/api/replication.storage/v1alpha1"
+
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+var mockVolumeReplicationClassObj = &replicationv1alpha1.VolumeReplicationClass{
+	ObjectMeta: metav1.ObjectMeta{
+		Name: "volume-replication-class",
+	},
+	Spec: replicationv1alpha1.VolumeReplicationClassSpec{
+		Provisioner: "test-driver",
+	},
+}
+
+func TestGetVolumeReplicaClass(t *testing.T) {
+	t.Parallel()
+	testcases := []struct {
+		createVrc       bool
+		errorExpected   bool
+		isErrorNotFound bool
+	}{
+		{createVrc: true, errorExpected: false, isErrorNotFound: false},
+		{createVrc: false, errorExpected: true, isErrorNotFound: true},
+	}
+
+	for _, tc := range testcases {
+		var objects []runtime.Object
+
+		volumeReplication := &replicationv1alpha1.VolumeReplication{}
+		mockVolumeReplicationObj.DeepCopyInto(volumeReplication)
+		objects = append(objects, volumeReplication)
+
+		if tc.createVrc {
+			volumeReplicationClass := &replicationv1alpha1.VolumeReplicationClass{}
+			mockVolumeReplicationClassObj.DeepCopyInto(volumeReplicationClass)
+			objects = append(objects, volumeReplicationClass)
+		}
+
+		reconciler := createFakeVolumeReplicationReconciler(t, objects...)
+		vrcObj, err := reconciler.getVolumeReplicationClass(log.FromContext(context.TODO()), mockVolumeReplicationClassObj.Name)
+
+		if tc.errorExpected {
+			assert.Error(t, err)
+			if tc.isErrorNotFound {
+				assert.True(t, errors.IsNotFound(err))
+			}
+		} else {
+			assert.NoError(t, err)
+			assert.NotEqual(t, nil, vrcObj)
+		}
+	}
+}
