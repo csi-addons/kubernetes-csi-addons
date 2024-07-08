4971f6561c3be97926239b3227b116cb3e29e69c
Nikhil-Ladha <nikhilladha1999@gmail.com>
2024-07-08 17:39:18 +0530

    internal: add sidecar service and client setup for volume group
    added sidecar service and client setup code for volume group
    feature
    
    Signed-off-by: Nikhil-Ladha <nikhilladha1999@gmail.com>

--- sidecar/main.go
+++ sidecar/main.go
@@ -115,6 +115,7 @@ func main() {
 	sidecarServer.RegisterService(service.NewNetworkFenceServer(csiClient.GetGRPCClient(), kubeClient))
 	sidecarServer.RegisterService(service.NewReplicationServer(csiClient.GetGRPCClient(), kubeClient))
 	sidecarServer.RegisterService(service.NewEncryptionKeyRotationServer(csiClient.GetGRPCClient(), kubeClient))
+	sidecarServer.RegisterService(service.NewVolumeGroupServer(csiClient.GetGRPCClient(), kubeClient))
 
 	isController, err := csiClient.HasControllerService()
 	if err != nil {
