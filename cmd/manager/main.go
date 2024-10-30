1b2121ea6898449381d39a1f6f848c0d68829e51
Madhu Rajanna <madhupr007@gmail.com>
2024-10-30 13:25:52 +0100

    api: add NetworkFenceClass CRD
    adding NetworkFenceClass which is
    going to be used to get the client
    details to be fenced.
    
    The controller will watch for the NetworkFenceClass
    CR and sends a periodic request to all
    the sidecars registered with the matching
    driver name that registered the
    GET_CLIENTS_TO_FENCE caps to get the client
    details, once the controller gets the client
    details its going to update the CSIAddonsNode
    object status with the client information that
    can be used to do Network fencing and unfencing.
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -234,6 +234,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "EncryptionKeyRotationCronJob")
 		os.Exit(1)
 	}
+
+	if err = (&controllers.NetworkFenceClassReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "NetworkFenceClass")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
