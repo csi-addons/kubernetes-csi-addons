509abd5ab4b3b852b69f7a53cb8687c6aee7fb6e
Madhu Rajanna <madhupr007@gmail.com>
2024-10-31 11:48:36 +0100

    use predictable name for csiaddons object
    This PR does the below changes
    
    * Use the deployment/Daemonset as the owner
    of the csiaddons instead of pod
    
    * Use predictable name which includes the
    nodeID, owner name and the kind to ensure
    that we get unique names per resources also
    if the pod is restarting we will reuse the
    same resource name for the csiAddonsNode object.
    Using Resource Kind as csiAddonsNode object is
    a namespaced resouces
    
    * Take care of worst case where resource char limit
    is not meet.
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- sidecar/internal/csiaddonsnode/csiaddonsnode_test.go
+++ sidecar/internal/csiaddonsnode/csiaddonsnode_test.go
@@ -17,15 +17,22 @@ limitations under the License.
 package csiaddonsnode
 
 import (
-	"reflect"
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"strings"
 	"testing"
 
 	csiaddonsv1alpha1 "github.com/csi-addons/kubernetes-csi-addons/api/csiaddons/v1alpha1"
 	"github.com/csi-addons/kubernetes-csi-addons/sidecar/internal/client"
 
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
+	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
 // mockClient implements a fake interface for the Client type.
@@ -60,104 +67,195 @@ func (mc *mockClient) HasControllerService() (bool, error) {
 
 func Test_getCSIAddonsNode(t *testing.T) {
 	var (
-		podName     = "pod"
-		podNamspace = "default"
-		podUID      = "123"
+		name      = "pod"
+		namespace = "default"
+		uid       = "123"
 	)
 
 	type args struct {
 		driverName string
 		endpoint   string
 		nodeID     string
+		kind       string
 	}
+
 	tests := []struct {
 		name    string
-		init    func()
 		args    args
 		want    *csiaddonsv1alpha1.CSIAddonsNode
 		wantErr bool
 	}{
 		{
-			name: "Test 1",
+			name: "Deployment Owner",
 			args: args{
 				driverName: "example.com",
 				endpoint:   "192.168.61.228:6060",
 				nodeID:     "123",
+				kind:       "Deployment",
 			},
-			want: &csiaddonsv1alpha1.CSIAddonsNode{
-				TypeMeta: v1.TypeMeta{},
-				ObjectMeta: v1.ObjectMeta{
-					Name:      podName,
-					Namespace: podNamspace,
-					OwnerReferences: []v1.OwnerReference{
-						{
-							APIVersion: "v1",
-							Kind:       "Pod",
-							Name:       podName,
-							UID:        types.UID(podUID),
-						},
-					},
-				},
-				Spec: csiaddonsv1alpha1.CSIAddonsNodeSpec{
-					Driver: csiaddonsv1alpha1.CSIAddonsNodeDriver{
-						Name:     "example.com",
-						EndPoint: "192.168.61.228:6060",
-						NodeID:   "123",
-					},
-				},
-			},
+			want:    createExpectedNode(namespace, uid, "example.com", "192.168.61.228:6060", "123", "Deployment", name),
 			wantErr: false,
 		},
 		{
-			name: "Test 2",
+			name: "DaemonSet Owner",
+			args: args{
+				driverName: "example.com",
+				endpoint:   "192.168.61.228:6060",
+				nodeID:     "123",
+				kind:       "DaemonSet",
+			},
+			want:    createExpectedNode(namespace, uid, "example.com", "192.168.61.228:6060", "123", "DaemonSet", name),
+			wantErr: false,
+		},
+		{
+			name: "IPv6 Endpoint",
 			args: args{
 				driverName: "csi.example.com",
 				endpoint:   "[2001:0db8:3c4d:0015:0000:0000:1a2f:1a2b]:8080",
 				nodeID:     "32532-1312-435354",
+				kind:       "Deployment",
 			},
-			want: &csiaddonsv1alpha1.CSIAddonsNode{
-				TypeMeta: v1.TypeMeta{},
-				ObjectMeta: v1.ObjectMeta{
-					Name:      podName,
-					Namespace: podNamspace,
-					OwnerReferences: []v1.OwnerReference{
-						{
-							APIVersion: "v1",
-							Kind:       "Pod",
-							Name:       podName,
-							UID:        types.UID(podUID),
-						},
-					},
-				},
-				Spec: csiaddonsv1alpha1.CSIAddonsNodeSpec{
-					Driver: csiaddonsv1alpha1.CSIAddonsNodeDriver{
-						Name:     "csi.example.com",
-						EndPoint: "[2001:0db8:3c4d:0015:0000:0000:1a2f:1a2b]:8080",
-						NodeID:   "32532-1312-435354",
-					},
-				},
-			},
+			want:    createExpectedNode(namespace, uid, "csi.example.com", "[2001:0db8:3c4d:0015:0000:0000:1a2f:1a2b]:8080", "32532-1312-435354", "Deployment", name),
 			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Setup manager with mock client and pod
+			mgr, err := setupManager(tt.args.driverName, name, namespace, uid, tt.args.kind)
+			if err != nil {
+				t.Errorf("Failed to setup manager: %v", err)
+			}
+			mgr.Endpoint = tt.args.endpoint
+			mgr.Node = tt.args.nodeID
+
+			got, err := mgr.getCSIAddonsNode()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getCSIAddonsNode() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			assert.Equal(t, tt.want, got, "getCSIAddonsNode() = %v, want %v", got, tt.want)
+		})
+	}
+}
+
+// setupManager initializes the Manager with the provided args.
+func setupManager(driverName, name, namespace, uid, kind string) (*Manager, error) {
+	mgr := &Manager{
+		Client:       NewMockClient(driverName),
+		KubeClient:   fake.NewSimpleClientset(),
+		PodName:      name,
+		PodNamespace: namespace,
+		PodUID:       uid,
+	}
+
+	_, err := mgr.KubeClient.CoreV1().Pods(namespace).Create(context.TODO(), &corev1.Pod{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			UID:       types.UID(uid),
+			OwnerReferences: []v1.OwnerReference{
+				{
+					APIVersion: "v1",
+					Kind:       kind,
+					Name:       name,
+					UID:        types.UID(uid),
+				},
+			},
+		},
+	}, v1.CreateOptions{})
+
+	return mgr, err
+}
+
+// createExpectedNode generates the expected CSIAddonsNode for the test.
+func createExpectedNode(namespace, uid, driverName, endpoint, nodeID, ownerKind, ownerName string) *csiaddonsv1alpha1.CSIAddonsNode {
+	// Generate the name dynamically based on the resource type
+	name := fmt.Sprintf("%s-%s-%s-%s", nodeID, namespace, strings.ToLower(ownerKind), ownerName)
+	return &csiaddonsv1alpha1.CSIAddonsNode{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			OwnerReferences: []v1.OwnerReference{
+				{
+					APIVersion: "v1",
+					Kind:       ownerKind,
+					Name:       "pod",
+					UID:        types.UID(uid),
+				},
+			},
+		},
+		Spec: csiaddonsv1alpha1.CSIAddonsNodeSpec{
+			Driver: csiaddonsv1alpha1.CSIAddonsNodeDriver{
+				Name:     driverName,
+				EndPoint: endpoint,
+				NodeID:   nodeID,
+			},
+		},
+	}
+}
+
+func Test_generateName(t *testing.T) {
+	type args struct {
+		nodeID    string
+		namespace string
+		ownerKind string
+		ownerName string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Normal case",
+			args: args{
+				nodeID:    "example-node-id",
+				namespace: "example-namespace",
+				ownerKind: "Deployment",
+				ownerName: "example-owner-name",
+			},
+		},
+		{
+			name: "Case with long names",
+			args: args{
+				nodeID:    "a-very-long-node-id-that-exceeds-the-limit-of-253-characters-and-should-trigger-uuid-generation-12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890",
+				namespace: "test-namespace",
+				ownerKind: "Deployment",
+				ownerName: "another-long-owner-name-that-is-definitely-more-than-253-characters-long-12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890",
+			},
+		},
+		{
+			name: "Exact 253 characters",
+			args: args{
+				nodeID:    "a-very-long-node-id-that-is-exactly-128-characters-long-because-of-some-conditions-and-requirements-conditionsandrequirements",
+				namespace: "test-namespace",
+				ownerKind: "Deployment",
+				ownerName: "another-long-owner-name-that-is-very-unique-and-specifies-exactly-how-the-naming-should-work-conditionss",
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			mgr := &Manager{
-				Client:       NewMockClient(tt.args.driverName),
-				Node:         tt.args.nodeID,
-				Endpoint:     tt.args.endpoint,
-				PodName:      "pod",
-				PodNamespace: "default",
-				PodUID:       "123",
+			want := generateExpectedLongName(tt.args.nodeID, tt.args.namespace, tt.args.ownerKind, tt.args.ownerName)
+			got, err := generateName(tt.args.nodeID, tt.args.namespace, tt.args.ownerKind, tt.args.ownerName)
+			if err != nil {
+				t.Errorf("generateName() error = %v", err)
 			}
-			got, err := mgr.getCSIAddonsNode()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("getCSIAddonsNode() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("getCSIAddonsNode() = %v, want %v", got, tt.want)
+			if got != want {
+				t.Errorf("generateName() = %v, want %v", got, want)
 			}
 		})
 	}
+}
+
+func generateExpectedLongName(nodeID, namespace, ownerKind, ownerName string) string {
+	ownerKind = strings.ToLower(ownerKind)
+	data := nodeID + namespace + ownerKind + ownerName
+	hash := sha256.Sum256([]byte(data))
+	uuid := hex.EncodeToString(hash[:8])
+	base := fmt.Sprintf("%s-%s-%s-%s", nodeID, namespace, ownerKind, ownerName)
+	if len(base) > 253 {
+		return fmt.Sprintf("%s-%s", base[:251-len(uuid)-1], uuid)
+	}
+	return base
 }
