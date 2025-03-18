aa2aff8ec225b4766b65c36241722c1e1505c6cd
Praveen M <m.praveen@ibm.com>
2025-03-18 13:35:23 +0530

    api: add volumeGroupAttributes field to VGRContent API
    This commit introduces `volumeGroupAttributes` field to
    `VolumeGroupReplicationContent` API.
    
    The field is intended to store key-value pairs derived
    from the VolumeGroupContext returned by CSI drivers in the
    CreateVolumeGroupResponse.
    
    Signed-off-by: Praveen M <m.praveen@ibm.com>

--- api/replication.storage/v1alpha1/volumegroupreplicationcontent_types.go
+++ api/replication.storage/v1alpha1/volumegroupreplicationcontent_types.go
@@ -57,6 +57,11 @@ type VolumeGroupReplicationContentSpec struct {
 	// +kubebuilder:validation:Required
 	VolumeGroupReplicationClassName string `json:"volumeGroupReplicationClassName"`
 
+	// volumeGroupAttributes holds the contextual information of the volume group.
+	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="field is immutable"
+	// +kubebuilder:validation:Optional
+	VolumeGroupAttributes map[string]string `json:"volumeGroupAttributes,omitempty"`
+
 	// Source specifies whether the volume group is (or should be) dynamically provisioned
 	// or already exists using the volumes listed here, and just requires a
 	// Kubernetes object representation.
